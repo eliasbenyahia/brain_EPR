b157cf39eccc15874d0966cb40d32da48cb7f74a
eliasbenyahia <eliasbenyahia@free.fr>
2024-07-30 16:19:59 +0200

    added pyswarm templates for full-brain and region wise analysis and simulations

---
+++ pyswarm_simu_template.py
@@ -0,0 +1,148 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+import numpy.linalg as LA
+import numpy.matlib
+import os
+import pickle
+import mne
+from mne.minimum_norm import read_inverse_operator
+import csv
+import scipy.interpolate
+import scipy.stats
+import pandas as pd
+
+# Import PySwarms
+import pyswarms as ps
+from pyswarms.utils.functions import single_obj as fx
+
+# Import pca and svd
+import sklearn.decomposition
+from sklearn.utils.extmath import randomized_svd
+from mne.source_estimate import _prepare_label_extraction
+
+
+import json 
+
+############################## functions ####################################
+
+
+def dLinear(data,pars):
+    dimension = data.shape[0]
+    pars_m = pars.reshape((dimension, dimension))
+    # print(pars_m.shape)
+    f = np.dot(pars_m, data)
+    # print('force shape: ', np.shape(f))
+    return f
+
+def compute_entropy_n(data, dt, n_iter=50, Np=200):
+
+    dout = data
+    dimension, length=np.shape(dout)
+    optimizer = ps.single.LocalBestPSO(n_particles=Np, dimensions=dimension**2, options=options)
+    data_mid = (data[:,1:] + data[:,:length-1])/2
+    data_diff = data[:,1:] - data[:,:length-1]
+
+    
+    def costLinear(pars):  
+        # print(data_diff.shape)
+        # print(dLinear(data_mid,pars).shape)
+        JJ=np.sum(dLinear(data_mid,pars)* data_diff, axis=0)
+        return 2*(np.mean(JJ))*(np.mean(JJ))/(np.var(JJ)*dt)
+    
+    
+    def costLinear2(pars):  
+        # print(pars.shape)
+        return 1/(costLinear(pars)+1)
+    
+    def fun4(x):
+    #return x[:,0]**2+(x[:,1]-2)**2
+        dime = Np
+        cosi=np.zeros(Np)
+        b=x
+        # print(b.shape)
+        for im in range(Np):
+            cosi[im]=costLinear2(b[im,:])
+        return cosi
+    
+    cost, pos = optimizer.optimize(fun4, iters=n_iter)
+    sigma = 1/cost-1
+    return sigma, pos, [data_mid, data_diff]
+
+
+################################# Run ####################################
+
+
+# optimization parameters
+
+options = {'c1': 0.5, 'c2': 0.3, 'w':0.9, 'k': 2, 'p': 2}
+Np=200
+n_iter = 500
+n_components= 2 # for PCA
+shifts = [0.25,0.5,0.75,1.0,1.5,2.0,2.5]
+dt = 1
+data = []
+df = pd.DataFrame()
+set = '1.5_35'
+# Paths to data
+
+dir_simu = "simu/perlin"
+dir_data = os.path.join(dir_simu, 'data')
+dir_simu_output = os.path.join(dir_simu, 'output')
+if not os.path.isdir(dir_simu_output):
+    os.mkdir(dir_simu_output)
+dir_output_allshifts = os.path.join(dir_simu_output, 'simplex_noise_' + set + '_2_0.5_36_std_1.0_1.5_allshifts_0.75_0.5_8.0_drive_10.0_30.0_transfer_50.0_0.25')
+if not os.path.isdir(dir_output_allshifts):
+    os.mkdir(dir_output_allshifts)
+
+output_filename = os.path.join(dir_output_allshifts, 'EPR_PCA' + str(n_components) + '_pyswarm_Np' + str(Np) + '_iter' + str(n_iter) + '.csv' )
+
+save_singular_values = False
+
+for s,shift in enumerate(shifts):
+
+    data_param = "simplex_noise_" + set + "_2_0.5_36_std_1.0_1.5_"+str(shift)+"_0.75_0.5_8.0_drive_10.0_30.0_transfer_50.0_0.25"
+    
+    dir_output_data = os.path.join(dir_simu_output, data_param)
+    dir_data_param = os.path.join(dir_data,data_param )
+           
+    for i in range(20):
+        print('shift: ', shift)
+        print('seed: ', i)
+        filename_rate = os.path.join(dir_data_param, "rate_simplex_noise_baseline_"+ str(i) +".bn")
+        if not os.path.isdir(dir_output_data):
+            os.mkdir(dir_output_data)
+        sv_filename = os.path.join(dir_output_data, 'sv_seed' + str(i) +'.pkl')
+        lsv_filename = os.path.join(dir_output_data, 'lsv_seed' + str(i) +'.pkl')
+        rsv_filename = os.path.join(dir_output_data, 'rsv_seed' + str(i) +'.pkl')
+        
+        
+        with open(filename_rate, 'rb') as f:
+            data = pickle.load(f)[0]
+
+        U, Sigma, VT = randomized_svd(data, 
+                        n_components=n_components,
+                        n_iter=5,
+                        random_state=None)
+        
+        
+        scale = np.linalg.norm(Sigma) / np.sqrt(len(data))
+        data_pca_flip=  scale * VT
+        sigma, pos,_= compute_entropy_n(data_pca_flip, dt, n_iter=n_iter, Np=Np)
+
+        df_simu = pd.DataFrame.from_dict({'sigma' :[sigma], 'shift' : [shift], 'seed': [i]})
+        df = pd.concat([df,df_simu], ignore_index=True)
+        if save_singular_values:
+            with open(sv_filename, 'wb') as f:
+                pickle.dump(Sigma, f)
+            with open(lsv_filename, 'wb') as f:
+                pickle.dump(U, f)
+            with open(rsv_filename, 'wb') as f:
+                pickle.dump(VT, f)
+        
+        
+# writing to csv file
+df.to_csv(output_filename)
+
+
+            
